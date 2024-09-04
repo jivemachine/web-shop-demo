3d4c278d50f602fffa008adeffd289c1bdd8403d
jivemachine <the.jeremy.cobb@gmail.com>
2024-09-04 15:34:26 -0500

    added livewire function to grab product data & added some styling

--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+<div class="grid grid-cols-4 gap-4 mt-12">
+    @foreach($this->products as $product)
+        <div>
+            <img src="{{ $product->image }}" alt="">
+            <h2 class="font-medium text-lg">{{ $product->name }}</h2>
+
+            <span class="text-gray-700 text-sm">{{ $product->price }}</span>
+        </div>
+    @endforeach
 </div>
