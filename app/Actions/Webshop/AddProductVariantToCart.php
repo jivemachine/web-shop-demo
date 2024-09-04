0fa21f6d415cb2ecd1356e57863e4488b9e5f697
jivemachine <the.jeremy.cobb@gmail.com>
2024-09-04 18:18:21 -0500

    updated logic when items are added to cart

--- app/Actions/Webshop/AddProductVariantToCart.php
+++ app/Actions/Webshop/AddProductVariantToCart.php
@@ -11,9 +11,12 @@ class AddProductVariantToCart
 {
     public function add($variantId)
     {
-        CartFactory::make()->items()->create([
-            'product_variant_id' => $variantId,
-            'quantity' => 1,
-        ]);
+        CartFactory::make()->items()->firstOrCreate(
+            [
+                'product_variant_id' => $variantId,
+            ],
+            [
+                'quantity' => 0,
+        ])->increment('quantity');
     }
 }
