8a771f68b03511a124b26c06b951217a810077d8
jivemachine <the.jeremy.cobb@gmail.com>
2024-09-04 16:13:10 -0500

    added computed memory function

---
+++ app/Livewire/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Product extends Component
+{
+    public $productId;
+
+    public function mount($productId)
+    {
+
+    }
+
+    public function getProductProperty()
+    {
+        return \App\Models\Product::findOrfail($this->productId);
+    }
+
+    public function render()
+    {
+        return view('livewire.product');
+    }
+}
