53b486be81d0cf645f6a7be361a597e5fece12c9
jivemachine <the.jeremy.cobb@gmail.com>
2024-09-05 16:05:32 -0500

    added subtotal function

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -5,6 +5,7 @@ namespace App\Models;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
@@ -12,6 +13,15 @@ use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 class CartItem extends Model
 {
     use HasFactory;
+
+    protected function subtotal(): Attribute
+    {
+        return Attribute::make(
+            get: function() {
+                return $this->product->price->multiply($this->quantity);
+            }
+        );
+    }
 
     public function product(): HasOneThrough
     {
